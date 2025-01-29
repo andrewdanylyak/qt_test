e7ed1e1590bca8906cc23a74ed46fb850b5d4a52
Andrii Danyliak <andriy.danyliak@droid-technologies.com>
2025-01-29 18:43:08 +0200

    Changed code

--- DroneControl/mainwindow.h
+++ DroneControl/mainwindow.h
@@ -1,16 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-// #include <QVideoWidget>
-// #include <QMediaPlayer>
-
-// #include <QUrl>
-// #include <QNetworkRequest>
-// #include <QProcess>
 #include <QMainWindow>
-
-// #include <gst/gst.h>
-// #include <gst/video/videooverlay.h>
 
 #include "DtParser.h"
 #include "DtVideoWidget.h"
@@ -25,14 +16,6 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
-    // QVideoWidget * videoWidget;
-
-    // QMediaPlayer * player0;
-
-    // QUrl url0;
-    // QNetworkRequest requestRtsp0;
-    // bool is_connected = false;
-
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
@@ -41,8 +24,8 @@ private slots:
     void addErrorMessage(const QString &message);
     void addWarningMessage(const QString &message);
     void addInfoMessage(const QString &message);
-    void on_mSettingsButton_clicked();
-    void on_pushButton_clicked();
+    void onPushButtonClicked();
+    void onSettingsClicked();
 
 signals:
     void stopAll();
@@ -53,8 +36,7 @@ private:
     QThread mThread;
     void stopThreads();
     Dt::VideoWidget mVideoWidget;
-    // GstElement *m_pipeline = nullptr;
-    // void PlayVideoStream(guintptr output, const QString &url);
-    // static void on_pad_added(GstElement *src, GstPad *new_pad, gpointer data);
+
+    void setupSlots();
 };
 #endif // MAINWINDOW_H
